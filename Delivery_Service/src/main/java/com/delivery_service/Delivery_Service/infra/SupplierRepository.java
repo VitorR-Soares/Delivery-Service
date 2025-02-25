6af3e606648d199b0b50a937a08285ccdb10884c
VitorR-Soares <vitorratollasoares@gmail.com>
2025-02-25 12:01:10 -0300

    Repository Query created

--- Delivery_Service/src/main/java/com/delivery_service/Delivery_Service/infra/SupplierRepository.java
+++ Delivery_Service/src/main/java/com/delivery_service/Delivery_Service/infra/SupplierRepository.java
@@ -3,12 +3,25 @@ package com.delivery_service.Delivery_Service.infra;
 import com.delivery_service.Delivery_Service.core.domain.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 
 @Repository
 public interface SupplierRepository  extends JpaRepository<Supplier, Long> {
+
+    @Query("""
+            SELECT s FROM Supplier s
+            WHERE ST_CONTAINS(f.coverageArea, ST_GeoFromText(:location, 4326))
+            ORDER BY ST_DistanceSphere(f.address, ST_GeoFromText(:location, 4326))
+            LIMIT 1
+            """
+    )
+    public Supplier findNearestSupplier(@Param("location") String location);
+
+
 
 
 
